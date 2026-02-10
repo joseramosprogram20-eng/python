b83c03966153aa895253a9c5f704b0596f10efc3
jose <joseramosprogram20@gmail.com>
2026-02-09 21:49:20 -0600

    commit inicial

---
+++ ifdpi.py
@@ -0,0 +1,8 @@
+edad = int(input("que edad yienes?"))
+if edad <= 12:
+    print("es un nino")
+if edad >= 18:
+    print("es un adulto joven")
+
+if edad >= 22 :
+    print("eres un adulto ")
---
+++ ifpytihon.py
@@ -0,0 +1,5 @@
+precio = int(input("cuanto cuesta ?"))
+if precio >=100:
+    print("tiene un descuento del 20%")
+else:
+    print("no tiene descuento ")
